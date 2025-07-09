82d705b875dac933959d11d3561b25946cfe4ab0
Nihat DUYSAK <nihatdy@gmail.com>
2025-07-09 22:35:32 +0300

    login componenti eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,20 @@ export default function App() {
     <>
       <h1>Note Taking</h1>
       <button className='fixBtn button-secondary'>Gonder</button>
+
+      <Login />
+    </>
+  )
+}
+
+function Login() {
+  return (
+    <>
+      <form>
+        <p><input type="text" placeholder='ad' /></p>
+        <p><input type="text" placeholder='soyad' /></p>
+        <button>Giriş</button>
+      </form>
     </>
   )
 }
