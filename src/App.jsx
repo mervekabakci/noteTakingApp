109aeb5ec3b5e95993fa460b7bde7574dabaa87c
TriosMerve <merve@trios.com.tr>
2025-07-09 22:19:35 +0300

    note taking project initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Note Taking</h1>
+      <button className='fixBtn button-secondary'>Gonder</button>
+    </>
+  )
+}
